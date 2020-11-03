d5ca5a0839951a23c6cc5f374fd2cf200d58058d
BelkovV <belkov.vv@phystech.edu>
2020-11-03 11:59:14 +0300

    Stages 1-2

--- solar_model.py
+++ solar_model.py
@@ -6,51 +6,54 @@ gravitational_constant = 6.67408E-11
 
 
 def calculate_force(body, space_objects):
-    """Вычисляет силу, действующую на тело.
+	"""Вычисляет силу, действующую на тело.
 
-    Параметры:
+	Параметры:
 
-    **body** — тело, для которого нужно вычислить дейстующую силу.
-    **space_objects** — список объектов, которые воздействуют на тело.
-    """
+	**body** — тело, для которого нужно вычислить дейстующую силу.
+	**space_objects** — список объектов, которые воздействуют на тело.
+	"""
 
-    body.Fx = body.Fy = 0
-    for obj in space_objects:
-        if body == obj:
-            continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+	body.Fx = body.Fy = 0
+	for obj in space_objects:
+		if body == obj:
+			continue  # тело не действует гравитационной силой на само себя!
+		r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+		coeff = gravitational_constant * body.m * obj.m / (r**3)
+		body.Fx += (obj.x - body.x) * coeff
+		body.Fy += (obj.y - body.y) * coeff
 
 
 def move_space_object(body, dt):
-    """Перемещает тело в соответствии с действующей на него силой.
+	"""Перемещает тело в соответствии с действующей на него силой.
 
-    Параметры:
+	Параметры:
 
-    **body** — тело, которое нужно переместить.
-    """
+	**body** — тело, которое нужно переместить.
+	"""
 
-    ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
-    body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+	ax = body.Fx/body.m
+	ay = body.Fy/body.m
+	body.Vx += ax*dt
+	body.Vy += ay*dt
+	body.x += body.Vx*dt
+	body.y += body.Vy*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
-    """Пересчитывает координаты объектов.
+	"""Пересчитывает координаты объектов.
 
-    Параметры:
+	Параметры:
 
-    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
-    **dt** — шаг по времени
-    """
+	**space_objects** — список оьъектов, для которых нужно пересчитать координаты.
+	**dt** — шаг по времени
+	"""
 
-    for body in space_objects:
-        calculate_force(body, space_objects)
-    for body in space_objects:
-        move_space_object(body, dt)
+	for body in space_objects:
+		calculate_force(body, space_objects)
+	for body in space_objects:
+		move_space_object(body, dt)
 
 
 if __name__ == "__main__":
-    print("This module is not for direct call!")
+	print("This module is not for direct call!")
